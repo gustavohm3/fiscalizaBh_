31cf81f76c1cd34066619dd075f6d82eb91ac3d3
Gustavo Henrique <126521748+gustavohm3@users.noreply.github.com>
2024-11-18 17:25:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const quiz = [
+    {
+        q: 'Qual é o principal objetivo do projeto Fiscaliza BH?',
+        options:['Realizar eventos culturais na cidade', 'Monitorar e promover a transparência nas ações públicas',
+            'Incentivar o turismo em Belo Horizonte', 'Fornecer assessoria jurídica gratuita'],
+        answer: '1'
+    }, 
+    {
+        q: 'Qual desses é um órgão responsável pela fiscalização de obras públicas em Belo Horizonte?',
+        options:['Copasa', 'SLU',
+            'Sudecap', 'Funarbe'],
+        answer: '2'
+    },
+    {
+        q: 'Em relação ao saneamento básico, qual empresa é responsável pelo fornecimento de água e coleta de esgoto em Belo Horizonte?',
+        options:['Cemig', 'Copasa',
+            'SLU', 'SUDECAP'],
+        answer: '1'
+    },
+    {
+        q: 'Qual canal oficial o cidadão de Belo Horizonte pode usar para fazer denúncias e sugestões diretamente à prefeitura?',
+        options:['Portal Reclame Aqui', 'Ouvidoria BH Resolve',
+            'Procon Estadual', 'Site do Ministério Público'],
+        answer: '1'
+    },
+    {
+        q: 'Qual desses temas é prioridade para o projeto Fiscaliza BH?',
+        options:['Incentivo ao esporte de alto rendimento', 'Desenvolvimento de projetos culturais',
+            'Fiscalização de gastos públicos e transparência', 'Promoção de eventos musicais'],
+        answer: '2'
+    },
+    {
+        q: 'Qual dessas áreas é considerada crítica em Belo Horizonte e costuma ser monitorada por projetos de fiscalização?',
+        options:['Drenagem urbana e prevenção de enchentes', 'Organização de eventos culturais',
+            'Programas de incentivo ao turismo', 'Parques e áreas de lazer privados'],
+        answer: '0'
+    },
+    {
+        q: 'Para melhorar o trânsito em Belo Horizonte, a prefeitura lançou projetos relacionados a: ',
+        options:['Ampliação de áreas de lazer e esportes', 'Construção de novos estádios',
+            'Investimento em transporte público e mobilidade urbana', 'Aumento das tarifas de estacionamento'],
+        answer: '2'
+    },
+    {
+        q: 'Qual desses projetos é comumente proposto para melhorar a infraestrutura urbana de BH em tempos de chuva intensa?',
+        options:['Expansão da coleta seletiva', 'Criação de novos parques',
+            'Melhorias na drenagem urbana e contenção de encostas', 'Expansão do setor hoteleiro'],
+        answer: '2'
+    },
+    {
+        q: 'Qual é o principal canal online que a população de Belo Horizonte pode usar para acessar informações sobre investimentos públicos na cidade?',
+        options:['Site da Receita Federal', 'Portal da Transparência da Prefeitura de BH',
+            'Site do IBGE', 'Jornal de Minas'],
+        answer: '1'
+    },
+    {
+        q: 'Como o cidadão pode participar ativamente do monitoramento das ações da prefeitura de BH?',
+        options:['Através de redes sociais da prefeitura', 'Acessando e denunciando no Fiscaliza BH',
+            'Indo a eventos culturais municipais', 'Apenas por meio de grupos de bairro'],
+        answer: '1'
+    }
+]
